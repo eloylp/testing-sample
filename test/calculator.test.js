272b41a4d4dfd90fe175385f3b98b8168bc991b5
eloylp <eloylpdev@gmail.com>
2018-12-29 16:34:38 +0100

    fixed type error.

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -5,7 +5,7 @@ const { sum } = require('./../src/calculator')
 describe('Testing weird calculator.', () => {
 
   testCases.forEach(({ sumA, sumB, expectedResult }) => {
-    it(`Must calculate that ${sumA} + ${sumB} must be ${expectedResult}`, () => {
+    it(`Must calculate that ${sumA} + ${sumB} must be equal to ${expectedResult}`, () => {
       expect(sum(sumA, sumB)).toBe(parseFloat(expectedResult))
     })
   })
